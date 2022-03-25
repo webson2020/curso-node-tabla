415c6cab55e23dc989a7348ee0ef78e10f95f6c9
Nicolás Rossetti <nrossettiossa@gmail.com>
2022-03-25 15:21:33 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const { crearArchivo } = require('./helpers/multiplicar');
+const argv = require('./config/yargs');
+require('colors');
+
+console.clear();
+
+
+crearArchivo( argv.b, argv.l, argv.h )
+    .then( nombreArchivo => console.log(nombreArchivo.rainbow, 'creado'.rainbow) )
+    .catch( err => console.log(err) );
---
+++ config/yargs.js
@@ -0,0 +1,31 @@
+const { option } = require('yargs');
+
+const argv = require('yargs')
+                .option('b', {
+                    alias: 'base',
+                    type: 'number',
+                    demandOption: true,
+                    describe: 'Es la base de la  tabla de multiplicar'
+                })
+                .option('h', {
+                    alias: 'hasta',
+                    type: 'number',
+                    dafault: 10,
+                    describe: 'Este es el número hast dónde quieres la tabla'
+                })
+                .option('l', {
+                    alias: 'listar',
+                    type: 'boolean',
+                    demandOption: true,
+                    default: false,
+                    describe: ' Muestra la tabla en consola'
+                })
+                .check( (argv, options) => { 
+                    if( isNaN( argv.b ) ) {
+                        throw 'La base debe ser un numero';
+                    }
+                    return true;
+                })
+                .argv;
+
+module.exports = argv;
